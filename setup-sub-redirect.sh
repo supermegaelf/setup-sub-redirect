0d5242325d024b09c79fb2db25278899fef4c03a
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-04-13 02:26:27 +0300

    Create setup-sub-redirect.sh

---
+++ setup-sub-redirect.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+read -p "Main domain: " domain
+
+cat > /etc/nginx/conf.d/redirect.conf << EOL
+server {
+    listen 443 ssl;
+    server_name redirect.$domain;
+
+    root /var/www/redirect;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    include /etc/nginx/snippets/ssl.conf;
+    include /etc/nginx/snippets/ssl-params.conf;
+}
+EOL
+
+nginx -t && systemctl restart nginx
+
+sudo mkdir -p /var/www/redirect
+wget -q https://raw.githubusercontent.com/supermegaelf/sub-redirect/main/index.html -O /var/www/redirect/index.html
+
+sudo chown -R www-data:www-data /var/www/redirect
+sudo chmod -R 755 /var/www/redirect
+
+echo "Replace domain for redirect with '$domain' in sub page file: /var/lib/marzban/templates/subscription/index.html"
+read -p "Proceed? (y/n): " confirm
+
+if [ "$confirm" != "y" ]; then
+    echo "Operation aborted."
+    exit 1
+fi
+
+marzban restart
